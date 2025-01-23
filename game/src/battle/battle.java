a6e3d54061619f7935d50158c42a5fc10f79b91d
enea hoxha <eneahoxha05@hotmail.com>
2025-01-23 23:19:10 +0100

    add random

--- game/src/battle/battle.java
+++ game/src/battle/battle.java
@@ -12,29 +12,29 @@ import java.util.Random ;
 
 
 public class battle {
-    public void Battle_acction(Navy1 navy1, Navy2 navy2){
+    public void Battle_acction(Navy1 navy1, Navy2 navy2) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome to the Ship Battle!");
-        int navy1Damage = navy1.captain1.attack(navy2);
-        int navy2Damage = navy2.captain2.attack(navy1);
         boolean continueGame = true;
-        while (continueGame)
-        { // Genera un ambiente casuale ad ogni turno
-            //baseSea ambienteAttuale = generambienterandom();
+        while (continueGame) {
+            System.out.println("Welcome to the Ship Battle!");
+            int navy1Damage = navy1.captain1.attack(navy2);
+            int navy2Damage = navy2.captain2.attack(navy1);
             //System.out.println("\nYou are now battling in: " + ambienteAttuale.getName());
+            int random = generambienterandom();
 
-            // Applica l'effetto dell'ambiente alle navi
-            //ambienteAttuale.appliccaeffetto(navy1);
-            //ambienteAttuale.appliccaeffetto(navy2);
-            while (navy1.getVita() > 0 && navy2.getVita() > 0)
-            {
+            if (random == 1) {
+                navy1.setVita(navy1.getVita() + 100);
+            }
+            if (random == 1) {
+                navy2.setVita(navy2.getVita() + 100);
+            }
+            while (navy1.getVita() > 0 && navy2.getVita() > 0) {
 
                 System.out.print("Choose your target (1 for " + navy1.getNome() + ", 2 for " + navy2.getNome() + "): ");
                 int target = scanner.nextInt();
 
                 // Ship 1 attacks
-                if(target == 1){
+                if (target == 1) {
                     System.out.println("\n" + navy1.getNome() + " (" + navy1.getVita() + " HP) vs " + navy2.getNome() + " (" + navy2.getVita() + " HP)");
                     navy2.setVita(navy2.getVita() - navy1Damage);
 
@@ -52,35 +52,30 @@ public class battle {
                 System.out.println("\n========================================\n");
 
                 // print kte dhe kut te shtypi 2 te sulmoj 2
-                if(target == 2){
+                if (target == 2) {
                     navy1.setVita(navy1.getVita() - navy2Damage);
                     System.out.println(navy2.getNome() + " attacks " + navy1.getNome() + " for " + navy2Damage + " damage.");
-                     // Check if ship 1 is destroyed
+                    // Check if ship 1 is destroyed
                     if (navy1.getVita() <= 0) {
                         System.out.println(navy2.getNome() + " wins!");
                         continueGame = false;
                         break;
-                  }
+                    }
                 }
             }
-            continueGame = false;
+            System.out.print("Do you ant to continiue ?  Yes : 1  , No : 0");
+            int decision = scanner.nextInt();
+            continueGame = decision == 1;
+
         }
         scanner.close();
     }
 
 
 
-//    private  generambienterandom() {
-//        Random random = new Random();
-//        int ambiente = random.nextInt(3);
-//        switch (ambiente) {
-//            case 0:
-//                return new Sea1("Mare Ghiacciato");
-//            case 1:
-//                return new Sea2("Arcipelago misterioso");
-//            case 2:
-//                return new Sea3("Fossa delle anime perdute");
-//            default:
-//                return new Sea1("Default Sea");
-//        }
-}}
+    private int generambienterandom() {
+        Random random = new Random();
+        return random.nextInt(3);
+
+    }
+}
